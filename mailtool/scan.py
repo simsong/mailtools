9629a53b3ddc9410e828de07e2fc6b8c9f28266b
Simson Garfinkel <simsong@acm.org>
2020-06-07 17:26:28 -0400

    initial stats

--- mailtool/scan.py
+++ mailtool/scan.py
@@ -10,8 +10,41 @@ import logging
 import mailbox
 import sys
 
+from collections import defaultdict
+
 assert sys.version>'3.0.0'
 
+class MailStats:
+    def __init__(self):
+        self.senders  = defaultdict(int)
+        self.subjects = defaultdict(int)
+        self.receivers = defaultdict(int)
+
+    def printTopN(self, title, statobj, N):
+        print(f"{title}:")
+        counts = [(b[1],b[0]) for b in statobj.items()]
+        for (ct,(value,key)) in enumerate(sorted(counts,reverse=True)):
+            print(value,key)
+            if ct>=N:
+                break
+        print("")
+            
+
+    def process_message(self,msg):
+        if 'To' in msg:
+            self.receivers[ msg['To']] += 1
+        if 'Cc' in msg:
+            self.receivers[ msg['Cc']] += 1
+        if 'From' in msg:
+            self.senders[ msg['To']] += 1            
+        if 'Subject' in msg:
+            self.subjects[ msg['Subject']] += 1
+
+    def report(self):
+        self.printTopN('Receivers',self.receivers,10)
+        self.printTopN('Senders',self.senders,10)
+        self.printTopN('Subjects',self.subjects,10)
+        
 
 def process_mailbox(M, cb):
     for message in M:
@@ -30,7 +63,7 @@ def scan_directory(dirname, cb):
     for (dirpath, dirnames, filenames) in os.walk(dirname):
         print(dirname)
         for fname in filenames:
-            process_file( os.path.join(dirpath, fname))
+            process_file( os.path.join(dirpath, fname), cb)
 
 def message_printer(message):
     print("{:20s} {:30s} {:40s}".format(message['date'],message['from'],message['subject']))
@@ -41,8 +74,10 @@ if __name__=="__main__":
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("path", nargs="*", help="Files or Directories")
     args = parser.parse_args()
+    ms = MailStats()
     for name in args.path:
         if os.path.isdir(name):
-            scan_directory(name, message_printer)
+            scan_directory(name, ms.process_message)
         else:
-            process_file(name, message_printer)
+            process_file(name, ms.process_message)
+    ms.report()
